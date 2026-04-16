c8f9b190061aafb9bed59aeec3edf274cb0f1537
Hemin Jeong <jhemin0415@gmail.com>
2026-04-16 16:17:12 +0900

    [Feat] 라우터 설정 오류 수정

--- auth/app/api/api.py
+++ auth/app/api/api.py
@@ -5,5 +5,5 @@ from .endpoints import patient
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-api_router.include_router(auth.router, prefix="/auth/patient", tags=["patient"])
-api_router.include_router(auth.router, prefix="/auth/user", tags=["user"])
+api_router.include_router(user.router, prefix="/auth/patient", tags=["patient"])
+api_router.include_router(patient.router, prefix="/auth/user", tags=["user"])
