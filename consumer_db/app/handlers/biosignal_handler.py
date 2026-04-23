d11cc25a1438a47f7a2b418dea6bfcbb6843d75e
Hemin Jeong <jhemin0415@gmail.com>
2026-04-23 17:08:34 +0900

    ecg 데이터 카프카 수신 후 db 저장

--- consumer_db/app/handlers/biosignal_handler.py
+++ consumer_db/app/handlers/biosignal_handler.py
@@ -1,4 +1,18 @@
 import logging
+import uuid
+from datetime import datetime
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.future import select
+from common.db.session import SessionLocal
+from common.core.security import compress_and_encrypt_int_list, compress_and_encrypt_float_list
+from clinical_manage.app.models.info import PatientProfile, PractitionerProfiles, GenderEnum
+from auth.app.models.auth import User, Patient
+from biosignal.app.models.biosignals import Biosignals
+from common.core.security import get_password_hash
+
+
+
 from common.schemas.events import BiosignalECGEvent, BiosignalPPGEvent, BiosignalRESPEvent
 
 logger = logging.getLogger(__name__)
@@ -11,13 +25,23 @@ async def handle_ecg_event(event_data: dict):
     Args:
         event_data: Event payload
     """
-    event = BiosignalECGEvent(**event_data)
-    logger.info(
-        f"ECG analysis - patient_id: {event.patient_id}, "
-        f"signal_length: {len(event.signal)}, "
-        f"timestamp: {event.timestamp}"
-    )
     logger.info(f"Biosignal event received: {event_data.get('event_type')}")
+    event = BiosignalECGEvent(**event_data)
+    compressed_and_encrypted_signal = compress_and_encrypt_int_list(event.signal)
+    try:
+        async with SessionLocal() as db:
+            signal = Biosignals(patient_id=event.patient_id, biosignal_data=compressed_and_encrypted_signal, biosignal_type=event.signal_type, recorded_at=event.timestamp)
+            db.add(signal)
+            await db.commit()
+
+
+
+    except Exception as e:
+        logger.error(f"Failed to handle biosignal event: {e}", exc_info=True)
+        raise
+
+
+
     # TODO: Implement biosignal event processing logic
     pass
 
