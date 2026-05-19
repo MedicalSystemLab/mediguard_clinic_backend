5264b4df35dfa1b452f31ff94163c7f691ae3e43
Hemin Jeong <jhemin0415@gmail.com>
2026-05-19 13:24:39 +0900

    사용자 프로필 조회 구간 제작

--- clinical_manage/app/api/endpoints/manage.py
+++ clinical_manage/app/api/endpoints/manage.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, status, Depends
 from fastapi.security import HTTPBearer
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from auth.app.models.auth import User
+from auth.app.schemas.auth import Patient
 from common.db.session import get_db
 from common.core.config import settings
 from common.core.auth import get_current_patient_id
@@ -17,7 +20,15 @@ async def get_profile(
     current_client_id: str = Depends(get_current_patient_id),
     db: AsyncSession = Depends(get_db)
 ):
+    user = db.execute(select(User).where(User.client_id == current_client_id))
+    user = user.scalar_one_or_none()
 
+    if user:
+        return {"client_id": current_client_id, "user": user}
 
+    user = db.execute(select(Patient).where(Patient.patient_id == current_client_id))
+    user = user.scalar_one_or_none()
+    if user:
+        return {"client_id": current_client_id, "user": user}
 
     return {"client_id": current_client_id}
