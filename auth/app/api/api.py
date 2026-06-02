7f1ad23574e2873bf1be3dcfc958488b3f193614
Hemin Jeong <jhemin0415@gmail.com>
2026-06-02 16:17:01 +0900

    Move admin patient API to clinical manage

--- auth/app/api/api.py
+++ auth/app/api/api.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter
-from .endpoints import admin_patient
 from .endpoints import auth
 from .endpoints import user
 from .endpoints import patient
 
 api_router = APIRouter()
-api_router.include_router(admin_patient.router, prefix="/admin/patient", tags=["admin-patient"])
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(patient.router, prefix="/auth/patient", tags=["patient"])
 api_router.include_router(user.router, prefix="/auth/user", tags=["user"])
--- clinical_manage/app/api/api.py
+++ clinical_manage/app/api/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from .endpoints import admin_department, admin_practitioner, admin_ward, favorite, manage
+from .endpoints import admin_department, admin_patient, admin_practitioner, admin_ward, favorite, manage
 
 api_router = APIRouter()
 api_router.include_router(manage.router, prefix="/manage", tags=["manage"])
@@ -13,6 +13,11 @@ api_router.include_router(
     admin_practitioner.router,
     prefix="/manage/admin/practitioners",
     tags=["admin-practitioners"],
+)
+api_router.include_router(
+    admin_patient.router,
+    prefix="/manage/admin/patient",
+    tags=["admin-patient"],
 )
 api_router.include_router(
     admin_ward.router,
